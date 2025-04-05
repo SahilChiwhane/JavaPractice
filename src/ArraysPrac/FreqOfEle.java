0bc9552eb01a8f2ae5f90beff45c3b9d5653899b
Chiwhane <chiwhane@amazon.com>
2025-04-05 16:44:31 +0530

    commit

--- src/ArraysPrac/FreqOfEle.java
+++ src/ArraysPrac/FreqOfEle.java
@@ -24,32 +24,29 @@ public class FreqOfEle {
 
 //      Sorting of an Array
         int temp;
-        for (int i = 0; i < size - 1; i++) {  // Outer loop
+        for(int i = 0; i < size - 1; i++) {
             boolean swapped = false;
-            // Inner loop to compare adjacent elements
-            for (int j = 0; j < size - 1 - i; j++) {  // Reduced inner loop range
-                if (arr[j] > arr[j + 1]) {  // Swap if the current element is greater than the next one
+            for(int j = 0; j < size - 1 - i; j++) {
+                if(arr[j] > arr[j + 1]) {
                     temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-                    swapped = true;  // Mark that a swap occurred
+                    swapped = true;
                 }
             }
-
-            // If no swaps were made in this pass, the array is already sorted
-            if (!swapped) {
+            if(!swapped) {
                 break;
             }
         }
 
-//      frequency of each element is an array
+//      Count frequency of elements of an array
         for(int i = 0; i < size; i++) {
             int count = 1;
             while(i < size - 1 && arr[i] == arr[i + 1]) {
                 i++;
                 count++;
             }
-            System.out.println(arr[i] + " : " + count);
+            System.out.println("Count of " + arr[i] + " : " + count);
         }
 
     }
